85ee4aef7806acdade2689c041439d9d29798e25
Zilfs <rheinlandcast@gmail.com>
2024-02-16 22:55:58 +0700

    feat: mark task as completed service

--- js/manageTask.js
+++ js/manageTask.js
@@ -5,15 +5,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const taskWrapper = document.getElementById('taskWrapper');
     const taskWrapperEmpty = document.getElementById('taskWrapperEmpty');
 
-    function displayAllTasks(){
+    function captalizrFirstChar(str){
+        if(!str) return str;
+
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    } 
+
+    function formDate(userFriendllyDate){
+        const date = new Date(userFriendllyDate);
+
+        const option = {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+        };
+
+        return date.toLocaleDateString('en-GB', option);
+    }
+
+    function displayAllTasks(tasks = existingTask){
         if(existingTask.length === 0){
             taskWrapper.className = 'hidden';
             console.log("no tasks available");
         }else{
+            taskWrapper.innerHTML = '';
             taskWrapperEmpty.className = 'hidden';
             console.log("tasks available");
 
-            existingTask.forEach(tasks => {
+            tasks.forEach(task => {
                 const itemTask = document.createElement('div');
                 itemTask.className = 'flex justify-between bg-white p-5 w-full rounded-3xl';
                 itemTask.innerHTML = `
@@ -24,17 +43,17 @@ document.addEventListener('DOMContentLoaded', () => {
                         <img src="img/icons/ghost.svg" alt="icon">
                     </div>
                     <div class="flex flex-col">
-                        <p class="font-bold text-lg leading-[27px]">${tasks.taskName}</p>
-                        <p class="text-sm leading-[21px] text-taskia-grey">Created at ${tasks.createdAt}</p>
+                        <p class="font-bold text-lg leading-[27px]">${captalizrFirstChar(task.taskName)}</p>
+                        <p class="text-sm leading-[21px] text-taskia-grey">Created at ${formDate(task.createdAt)}</p>
                     </div>
                 </div>
-                ${tasks.isCompleted === false ?  
+                ${task.isCompleted === false ?  
                     `<div class="flex gap-4 font-semibold text-sm leading-[21px]">
                     <div class="flex gap-1 items-center">
                        <div class="flex shrink-0 w-5 h-5">
                            <img src="img/icons/layer.svg" alt="icon">
                        </div>
-                      <p>${tasks.taskPriority}</p>
+                      <p>${task.taskPriority}</p>
                     </div>
                     <div class="flex gap-1 items-center">
                         <div class="flex shrink-0 w-5 h-5">
@@ -53,10 +72,17 @@ document.addEventListener('DOMContentLoaded', () => {
                     </div>
                 </div>`
                 :
-                    `<div class="flex gap-1 items-center text-taskia-green">
+                `<div class="flex gap-4 font-semibold text-sm leading-[21px]">
+                    <div class="flex gap-1 items-center">
+                       <div class="flex shrink-0 w-5 h-5">
+                           <img src="img/icons/layer.svg" alt="icon">
+                       </div>
+                      <p>${task.taskPriority}</p>
+                    </div>
+                    <div class="flex gap-1 items-center text-taskia-green">
                         <div class="flex shrink-0 w-5 h-5">
                             <svg width="20" height="21" viewBox="0 0 20 21" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
+                            xmlns="http://www.w3.org/2000/svg">
                             <path d="M4.29163 2.16663V18.8333" stroke="currentColor" stroke-width="2"
                                 stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
                             <path
@@ -66,21 +92,30 @@ document.addEventListener('DOMContentLoaded', () => {
                             </svg>
                         </div>
                         <p>Completed</p>
-                    </div>`
+                    </div>
+                </div>`
                 }
                 <div class="flex flex-row items-center gap-x-3">
                     <a href="#"
                         class="my-auto font-semibold text-taskia-red border border-taskia-red p-[12px_20px] h-12 rounded-full">Delete</a>
-                    <a href="#"
-                        class="flex gap-[10px] justify-center items-center text-white p-[12px_20px] h-12 font-semibold bg-gradient-to-b from-[#977FFF] to-[#6F4FFF] rounded-full w-full border border-taskia-background-grey">Complete</a>
+                        ${task.isCompleted === false ?  
+                            `<a href="#" id="completeTask-${task.id}"
+                            class="flex gap-[10px] justify-center items-center text-white p-[12px_20px] h-12 font-semibold bg-gradient-to-b from-[#977FFF] to-[#6F4FFF] rounded-full w-full border border-taskia-background-grey">Complete</a>`
+                            :
+                             `<a href="#" id="completeTask-${task.id}" class="hidden"></a>`
+                        }
                 </div>
-            `;
+                `;
 
-            taskWrapper.appendChild(itemTask);
+                taskWrapper.appendChild(itemTask);
 
-            });
-
-            
+                itemTask.querySelector(`#completeTask-${task.id}`).addEventListener('click', function(event) {
+                    event.preventDefault();
+                    myTasks.completeTask(task.id);
+                    const updateTasks = myTasks.getTasks();
+                    displayAllTasks(updateTasks);
+                });
+            });            
         }
     }
 
